5739d1d121a5e9a91bfb7f13e97a8a88a6fc63be
madamenich <huotnich@pukyong.ac.kr>
2023-10-19 17:46:29 +0900

    cifar10 dataset

--- datasetloader/datalaoders.py
+++ datasetloader/datalaoders.py
@@ -1,8 +1,10 @@
-import torchvision.transforms as transforms
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Load the MNIST dataset
 
@@ -31,21 +33,13 @@ def get_mnist_dataset(batch_size=100, labels=None):
 
 
 
-def get_cifar10_dataset(batch_size=100, labels=None):
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+def get_cifar10_dataset(batch_size=4,labels=None):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=2)
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                             shuffle=False, num_workers=2)
-
-
-
-    return train_loader, test_loader
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
